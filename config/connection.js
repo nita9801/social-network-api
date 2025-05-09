0efeeee99dfe08297d673e25e602af86d0651c8d
nita9801 <187074331+nita9801@users.noreply.github.com>
2025-05-09 08:11:12 -0400

    reademe update

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,8 @@
-require('mongoose');
-const dotenv = require('dotenv');
+const mongoose = require("mongoose");
 
-dotenv.config();
+const connectionString =
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialNetworkDB";
 
-// database creation
+mongoose.connect(connectionString);
 
-const db = async () => {
-    try {
-        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB');
-        console.log('Database connected.');
-        return mongoose.connection;
-    } catch(error) {
-        console.error('Database connection error:', error);
-        throw new Error('Database connection failed.');
-    }
-}
-
-module.exports = db;
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -1,24 +1,16 @@
-// imports
-const mongoose = require('mongoose');
-const express = require('express');
+const express = require("express");
 const db = require("./config/connection");
-const dotenv = require('dotenv');
-const routes = require('./routes');
+const routes = require("./routes");
 
-dotenv.config();
-
-const app = express();
 const PORT = process.env.PORT || 3001;
-// middleware
-app.use(express.json());
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI,);
-mongoose.connection.once('open', () => {
-  console.log('Connected to MongoDB');
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
 });
-
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
